818b11b1df17d0f550408c88681d15ce11abb776
neotronix <36370548+neotronix@users.noreply.github.com>
2022-10-30 12:46:26 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>АО «Конструкторское бюро «Кунцево»</title>
+</head>
+
+<body>
+<a href="http://www.kbkuntsevo.ru/"><img src="/img/logo_5.jpg" width="253" height="169" alt=""></a><H1>&nbsp;Сайт на реконструкции</H1>
+
+</body>
+</html>
